23517083334f32f1d23c5b776121c07272a88b48
sprokaev <prokaevsergey@mail.ru>
2022-02-02 23:43:03 +0500

    added tests

--- src/test/java/com/demowebshop/tests/api/ApparelAndShoesPageTests.java
+++ src/test/java/com/demowebshop/tests/api/ApparelAndShoesPageTests.java
@@ -1,33 +1,14 @@
 package com.demowebshop.tests.api;
 
-import com.demowebshop.models.api.BlueAndGreenSneakerModel;
-import com.demowebshop.tests.TestBase;
 import com.demowebshop.tests.api.spec.Specification;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-
-public class ApparelAndShoesPageTests {
+public class ApparelAndShoesPageTests extends Specification {
 
     @Test
     @DisplayName("Добавление кед в корзину, без указания цвета и размера")
-    void addBlueAndGreenSneakerToCartTest() {
-        BlueAndGreenSneakerModel blueAndGreenSneakerModel = new BlueAndGreenSneakerModel();
-        blueAndGreenSneakerModel.setAddtocart_28_EnteredQuantity(1);
-
-        Response response =
-                given()
-                        .spec(Specification.request)
-                        .body(blueAndGreenSneakerModel)
-                        .when().post("/details/28/1")
-                        .then().spec(Specification.responseOk)
-                        .body("success", is(false))
-                        .extract().response();
-        assertThat(response.asString(), is(notNullValue()));
+    public void addBlueAndGreenSneakerToCartTest() {
+        apiSteps.addBlueAndGreenSneakerToCart();
     }
 }
--- src/test/java/com/demowebshop/tests/api/JewelryPageTests.java
+++ src/test/java/com/demowebshop/tests/api/JewelryPageTests.java
@@ -1,15 +1,10 @@
 package com.demowebshop.tests.api;
 
-import com.demowebshop.tests.TestBase;
 import com.demowebshop.tests.api.spec.Specification;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.core.Is.is;
-
-public class JewelryPageTests extends TestBase {
+public class JewelryPageTests extends Specification {
 
     @Test
     @DisplayName("Добавление кулона в корзину")
