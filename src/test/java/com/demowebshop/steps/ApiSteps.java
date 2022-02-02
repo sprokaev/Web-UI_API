4462505b3aa3eb9fdeb3d91961971760ab2db7d5
sprokaev <prokaevsergey@mail.ru>
2022-02-02 23:43:21 +0500

    added steps

--- src/test/java/com/demowebshop/steps/ApiSteps.java
+++ src/test/java/com/demowebshop/steps/ApiSteps.java
@@ -1,8 +1,10 @@
 package com.demowebshop.steps;
 
+import com.demowebshop.models.api.BlueAndGreenSneakerModel;
 import com.demowebshop.tests.api.spec.Specification;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
@@ -30,6 +32,19 @@ public class ApiSteps {
                 .body("success", is((true)))
                 .body("message", is(("The product has been added to your <a href=\"/cart\">shopping cart</a>")))
                 .body("updatetopcartsectionhtml", is(("(1)")))
+                .extract().response();
+    }
+
+    @Step("Добавление кед в корзину, без указания цвета и размера")
+    public Response addBlueAndGreenSneakerToCart() {
+        BlueAndGreenSneakerModel blueAndGreenSneakerModel = new BlueAndGreenSneakerModel();
+        blueAndGreenSneakerModel.setAddtocart_28_EnteredQuantity(1);
+
+        return given()
+                .spec(Specification.request)
+                .when().post("/details/28/1")
+                .then().spec(Specification.responseOk)
+                .body("success", Matchers.is(false))
                 .extract().response();
     }
 }
