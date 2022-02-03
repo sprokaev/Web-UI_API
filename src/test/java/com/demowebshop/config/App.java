40f940d158982a8b019b522d584a180f9c61d293
sprokaev <prokaevsergey@mail.ru>
2022-02-03 23:34:58 +0500

    fix

--- src/test/java/com/demowebshop/config/App.java
+++ src/test/java/com/demowebshop/config/App.java
@@ -7,7 +7,7 @@ public class App {
     public static final AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
 
     public static boolean isRemoteWebDriver() {
-        return !config.remoteDriverUrl().equals("");
+        return !config.remoteDriverUrl().equals("https://user1:1234@selenoid.autotests.cloud/wd/hub/");
     }
 
     public static boolean isVideoOn() {
