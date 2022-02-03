a1f63ac333e57e66ead470e574d6f4e737e82d9a
sprokaev <prokaevsergey@mail.ru>
2022-02-03 23:23:12 +0500

    fix

--- src/test/java/com/demowebshop/tests/TestBase.java
+++ src/test/java/com/demowebshop/tests/TestBase.java
@@ -6,7 +6,6 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demowebshop.config.App;
 import com.demowebshop.config.AppConfig;
 import com.demowebshop.helpers.AllureAttachments;
-import com.demowebshop.helpers.Attachments;
 import com.demowebshop.helpers.DriverUtils;
 import com.demowebshop.steps.ApiSteps;
 import com.demowebshop.steps.UiSteps;
@@ -16,6 +15,7 @@ import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 @ExtendWith({AllureJunit5.class})
@@ -25,6 +25,7 @@ public class TestBase {
 
     protected ApiSteps apiSteps = new ApiSteps();
     protected UiSteps uiSteps = new UiSteps();
+
 
     @BeforeAll
     static void setUp() {
@@ -36,12 +37,22 @@ public class TestBase {
 
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-infobars");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--lang=en-en");
+
+
         if (App.isRemoteWebDriver()) {
             capabilities.setCapability("enableVNC", true);
             capabilities.setCapability("enableVideo", true);
             Configuration.remote = App.config.remoteDriverUrl();
         }
 
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         Configuration.browserCapabilities = capabilities;
     }
 
