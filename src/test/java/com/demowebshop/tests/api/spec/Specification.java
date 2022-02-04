39e8332b80c13a0b5dac1fb18a7129ff819ccca0
sprokaev <prokaevsergey@mail.ru>
2022-02-04 23:53:45 +0500

    added baseUri(config.baseURI())

--- src/test/java/com/demowebshop/tests/api/spec/Specification.java
+++ src/test/java/com/demowebshop/tests/api/spec/Specification.java
@@ -1,25 +1,28 @@
 package com.demowebshop.tests.api.spec;
 
+import com.demowebshop.config.ProjectConfig;
 import com.demowebshop.steps.ApiSteps;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import org.aeonbits.owner.ConfigFactory;
 
-import static com.demowebshop.filter.CustomLogFilter.customLogFilter;
+import static com.demowebshop.helpers.AllureRestAssuredFilter.customLogFilter;
 import static io.restassured.RestAssured.with;
 
 
 public class Specification {
 
+    public static final ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
     public ApiSteps apiSteps = new ApiSteps();
 
     public static RequestSpecification request = with()
             .filter(customLogFilter().withCustomTemplates())
             .log().all()
             .contentType(ContentType.JSON)
-            .baseUri("http://demowebshop.tricentis.com")
+            .baseUri(config.baseURI())
             .basePath("/addproducttocart");
 
     public static ResponseSpecification responseOk = new ResponseSpecBuilder()
