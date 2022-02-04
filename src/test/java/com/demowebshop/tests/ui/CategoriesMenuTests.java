7c08ef71517c897fe7a1e5813b235db7f2ee18f2
sprokaev <prokaevsergey@mail.ru>
2022-02-04 23:52:56 +0500

    added test

---
+++ src/test/java/com/demowebshop/tests/ui/CategoriesMenuTests.java
@@ -0,0 +1,21 @@
+package com.demowebshop.tests.ui;
+
+import com.demowebshop.models.ui.CategoriesMenuEnum;
+import com.demowebshop.tests.TestBase;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static io.qameta.allure.Allure.parameter;
+
+public class CategoriesMenuTests extends TestBase {
+
+    @ParameterizedTest(name = "Проверка отображения ссылки {0} в блоке CATEGORIES")
+    @EnumSource(CategoriesMenuEnum.class)
+    void SearchProductTest(CategoriesMenuEnum categoriesMenuEnum) {
+        parameter("Название направления", categoriesMenuEnum);
+        parameter("Код направления", categoriesMenuEnum.linkName);
+
+        uiSteps.openUrl("");
+        uiSteps.findLinkName(categoriesMenuEnum.linkName);
+    }
+}
