c64b028b4fc20ba00011c4fe1f1f6a8e077fcc54
sprokaev <prokaevsergey@mail.ru>
2022-02-05 02:13:30 +0500

    optimise imports

--- src/test/java/com/demowebshop/tests/ui/CategoriesMenuTests.java
+++ src/test/java/com/demowebshop/tests/ui/CategoriesMenuTests.java
@@ -2,9 +2,14 @@ package com.demowebshop.tests.ui;
 
 import com.demowebshop.models.ui.CategoriesMenuEnum;
 import com.demowebshop.tests.TestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.parameter;
 
 public class CategoriesMenuTests extends TestBase {
@@ -12,8 +17,8 @@ public class CategoriesMenuTests extends TestBase {
     @ParameterizedTest(name = "Проверка отображения ссылки {0} в блоке CATEGORIES")
     @EnumSource(CategoriesMenuEnum.class)
     void SearchProductTest(CategoriesMenuEnum categoriesMenuEnum) {
-        parameter("Название направления", categoriesMenuEnum);
-        parameter("Код направления", categoriesMenuEnum.linkName);
+        parameter("Наименование категории товаров", categoriesMenuEnum);
+        parameter("Наименование категории товаров", categoriesMenuEnum.linkName);
 
         uiSteps.openUrl("");
         uiSteps.findLinkName(categoriesMenuEnum.linkName);
