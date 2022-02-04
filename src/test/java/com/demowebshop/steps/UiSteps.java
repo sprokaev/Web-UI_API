022e682c533bf78c3a052f93ca9bf27e5c921342
sprokaev <prokaevsergey@mail.ru>
2022-02-05 00:34:41 +0500

    fix

--- src/test/java/com/demowebshop/steps/UiSteps.java
+++ src/test/java/com/demowebshop/steps/UiSteps.java
@@ -5,8 +5,7 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.Cookie;
 
 import static com.codeborne.selenide.CollectionCondition.texts;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
@@ -37,7 +36,7 @@ public class UiSteps {
 
     @Step("Проверка отображения ссылки '{linkName}' в блоке CATEGORIES")
     public void findLinkName(String linkName) {
-        $$(".list").shouldHave(texts(linkName));
+        $$(".list").findBy(exactText(linkName));
     }
 }
 
