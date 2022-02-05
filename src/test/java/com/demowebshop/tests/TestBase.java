2c2b56a1e5b4dbdd067cbb22f2bcac36c6f450ee
sprokaev <prokaevsergey@mail.ru>
2022-02-05 20:07:51 +0500

    setting for browser moved DriverSettings class

--- src/test/java/com/demowebshop/tests/TestBase.java
+++ src/test/java/com/demowebshop/tests/TestBase.java
@@ -1,22 +1,17 @@
 package com.demowebshop.tests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.demowebshop.config.ProjectConfig;
 import com.demowebshop.helpers.AllureAttachments;
+import com.demowebshop.helpers.DriverSettings;
 import com.demowebshop.steps.ApiSteps;
 import com.demowebshop.steps.UiSteps;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
-
-    public static final ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
 
     protected ApiSteps apiSteps = new ApiSteps();
     protected UiSteps uiSteps = new UiSteps();
@@ -24,14 +19,7 @@ public class TestBase {
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.startMaximized = true;
-        Configuration.baseUrl = config.baseUrl();
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-        Configuration.remote = System.getProperty("remoteUrl");
+        DriverSettings.configure();
     }
 
     @AfterEach
