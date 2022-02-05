1ae34c24b073886262184feb0b6d7ae96a1f8fd5
sprokaev <prokaevsergey@mail.ru>
2022-02-05 13:46:57 +0500

    edit parameter in test

--- src/test/java/com/demowebshop/tests/ui/CategoriesMenuTests.java
+++ src/test/java/com/demowebshop/tests/ui/CategoriesMenuTests.java
@@ -2,14 +2,9 @@ package com.demowebshop.tests.ui;
 
 import com.demowebshop.models.ui.CategoriesMenuEnum;
 import com.demowebshop.tests.TestBase;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.parameter;
 
 public class CategoriesMenuTests extends TestBase {
@@ -17,7 +12,6 @@ public class CategoriesMenuTests extends TestBase {
     @ParameterizedTest(name = "Проверка отображения ссылки {0} в блоке CATEGORIES")
     @EnumSource(CategoriesMenuEnum.class)
     void SearchProductTest(CategoriesMenuEnum categoriesMenuEnum) {
-        parameter("Наименование категории товаров", categoriesMenuEnum);
         parameter("Наименование категории товаров", categoriesMenuEnum.linkName);
 
         uiSteps.openUrl("");
