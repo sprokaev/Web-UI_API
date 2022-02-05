1167655d1e9be52986d985507e98f4c06456a73b
sprokaev <prokaevsergey@mail.ru>
2022-02-05 20:06:01 +0500

    added new parameters

--- src/test/java/com/demowebshop/config/ProjectConfig.java
+++ src/test/java/com/demowebshop/config/ProjectConfig.java
@@ -2,18 +2,41 @@ package com.demowebshop.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"classpath:config/env.properties"})
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:config/env.properties"
+})
 public interface ProjectConfig extends Config {
 
     @Key("baseUrl")
+    @DefaultValue("http://demowebshop.tricentis.com")
     String baseUrl();
 
     @Key("baseURI")
+    @DefaultValue("http://demowebshop.tricentis.com")
     String baseURI();
 
+    @DefaultValue("chrome")
+    String browser();
+
+    @DefaultValue("91.0")
+    String browserVersion();
+
+    @DefaultValue("1920x1080")
+    String browserSize();
+
     @Key("remoteDriverUrl")
+    @DefaultValue("")
     String remoteDriverUrl();
 
     @Key("videoStorage")
+    @DefaultValue("")
     String videoStorage();
+
+    @Key("login")
+    String login();
+
+    @Key("password")
+    String password();
 }
